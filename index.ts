2faa27346b319fd8fdca52468653982aed79ad93
Dan Reale <dan.reale@semanticbits.com>
2020-06-04 22:15:06 -0400

    fixed dependecy

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
 export const danclick = (element: string): void => {
   cy.get(element).click()
 }
+export const haveText = (element: string, text: string) => {
+  cy.get(element).should('have.text', text)
+}
